dcf681c625729d1f7634b121ad33cb1ad0cd140e
James Cockram <jamesccockram16@gmail.com>
2019-02-14 15:08:44 +1100

    Update main.lua

--- main.lua
+++ main.lua
@@ -15,15 +15,15 @@ CenterY = display.contentCenterY
 local Title = display.newText("My Fitness Pal", CenterX, 10, native.systemFontBold, 42)
 local yourData = display.newText("Your Data", CenterX, 70, native.systemFontBold, 24)
 local BMITitle = display.newText("Your BMI", CenterX, 340, native.systemFontBold, 36)
-local BMIOutputNumber = display.newText("BMI", CenterX, 400, native.systemFontBold, 30)
-local BMIOutputText = display.newText("Category", CenterX, 450, native.systemFontBold, 28)
+local lblBMI = display.newText("BMI", CenterX, 400, native.systemFontBold, 30)
+local lblCatergory = display.newText("Category", CenterX, 450, native.systemFontBold, 28)
 local titleHeight = display.newText("Weight:", 70, 190, native.systemFontBold, 20)
 local titleWeight = display.newText("Height:", 70, 130, native.systemFontBold, 20)
 
-BMIOutputNumber:setFillColor(1,1,1 , 0.5)
-BMIOutputText:setFillColor(1,1,1, 0.5)
+lblBMI:setFillColor(1,1,1 , 0.5)
+lblCatergory:setFillColor(1,1,1, 0.5)
 
---Text Box
+--Text Boxes
 local txtBxHeight = native.newTextField(210, 130, 150, 40)
 
 txtBxHeight.font = native.newFont(systemFontBold, 24)
@@ -32,45 +32,45 @@ txtBxHeight.placeholder = "Height (M)"
 
 local txtBxWeight = native.newTextField(210, 190, 150, 40)
 
-txtBxWeight.inputType = "number"
-
 txtBxWeight.font = native.newFont(systemFontBold, 24)
 txtBxWeight:resizeHeightToFitFont()
 txtBxWeight.placeholder = "Weight (KG)"
 
+--Event Handlers
+function handleButtonEvent( event )
+	print(event.phase)
+	--If button is clicked
+	if event.phase == "ended" then
+	--Get User Data
+	height = tonumber(txtBxHeight.text)
+	weight = tonumber(txtBxWeight.text)
 
-
---Touch Listener
-local function handleButtonEvent( event )
-	-- body
-	if ("ended" == event.phase ) then
-		height = txtBxHeight.text 	
-		weight = txtBxWeight.text
-	if event.target.name == "calculate" then
-		BMI = weight/height/height
-		BMI = math.round(BMI)
-
-	if BMI <18 then
-		BMIOutputText.text = ("Underweight")
-		BMIOutputText:setTextColor(0,0,1)
-		BMIOutputNumber.text = (BMI)
-	elseif BMI <25 then
-		BMIOutputText.text = ("Normal")
-		BMIOutputText:setTextColor(0,1,0)
-		BMIOutputNumber.text = (BMI)
-	elseif BMI <30 then
-		BMIOutputText.text = ("Overweight")
-		BMIOutputText:setTextColor(1,1,0)
-		BMIOutputNumber.text = (BMI)
+	if height == nil or weight == nil then
+		native.showAlert("Error", "Invalid Data", {"OK", "Cancel"})
+	elseif height <1 or height >3 then
+		native.showAlert("Error", "Height must be between 1 and 3 Meters",{"OK", "Cancel"})
 	else
-		BMIOutputText.text = ("Obese")
-		BMIOutputText:setTextColor(1,0,0)
-		BMIOutputNumber.text = (BMI)
+		--Calculate BMI
+		bmi = weight/(height ^2)
+		bmi = math.round(bmi*10)/10
+		lblBMI.text = bmi
+
+		if bmi <18.5 then
+			lblCatergory.text = "Underweight"
+			lblCatergory:setFillColor(0,0,1)
+		elseif bmi < 24 then
+			lblCatergory.text = "Normal"
+			lblCatergory:setFillColor(0,1,0)
+		elseif bmi < 30 then
+			lblCatergory.text = "Overweight"
+			lblCatergory:setFillColor(0,1,1)
+		else
+			lblCatergory.text = "Obese"
+			lblCatergory:setFillColor(1,0,0)
+		end
 	end
 end
 end
-end
-
 --Calculate Button
 local btnCalculate = widget.newButton(
 {
